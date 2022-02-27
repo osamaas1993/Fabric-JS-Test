911b331883179e2fb2c0fa2133d0aa52445f65b3
osamaas1993 <100147939+osamaas1993@users.noreply.github.com>
2022-02-27 12:07:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,121 @@
 console.log ("This is javascript")
 
-/* const canvas = new fabric.Canvas('canvas', {
-    width: 500,
-    height: 500,
-   
-    isDrawingMode: true,
-    });
-
-fabric.Image.fromURL ('https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_960_720.jpg', (img) => {
-    canvas.backgroundImage = img 
-    canvas.renderAll()
-})
-*/
-
-
+//function for creating the canvas, the function includes its parameters
 function initCanvas(id) {
     return new fabric.Canvas(id, {
-        width: 500,
-        height: 500,
+        width: 800,
+        height: 600,
         isDrawingMode: false,
         selection : false,
+        selectionKey : ["shiftKey"],
+        skipTargetFind : false,
     });
 }
 
+//setting the canvas background through URL
 function setBackground(url, canvas) {
     fabric.Image.fromURL(url, function (img) {
             canvas.backgroundImage = img;
             canvas.renderAll();
         });
 }
-const canvas = initCanvas ('canvas');
+
+//creating a canvas that takes its options from the initCanvas function, the ID parameter is the canvas html ID
+const canvas = initCanvas ('canvasID');
+
+//modes objects and currentMode variable
+const modes = {
+    pan : 'panMode',
+    build : 'buildMode',
+    draw : 'drawMode',
+};
+
+let currentMode = 'buildMode';
 let mousePressed = false;
 
-
-let currentMode = '';
-const modes = {
-    pan : 'pan'
-    };
-
+//for the toggle pan button
 function togglePan() {
-    if (currentMode === 'pan') {
-        currentMode = '';
+    if (currentMode !== 'panMode') {
+        currentMode = 'panMode';
+        canvas.isDrawingMode = false;
+        canvas.skipTargetFind = true;
         console.log(currentMode);
         }
     else {
-        currentMode = 'pan',
+        currentMode = 'buildMode',
         console.log(currentMode);
         }
 };
 
-setBackground ('https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_960_720.jpg', canvas)
+//for the toggle build/select button
+function toggleBuild() {
+    if (currentMode !== 'buildMode') {
+        currentMode = 'buildMode';
+        canvas.skipTargetFind = false;
+        canvas.isDrawingMode = false;
+        console.log(currentMode);
+        }
+};
+
+//for the toggle draw button
+function toggleDraw() {
+    if (currentMode !== 'drawMode') {
+        currentMode = 'drawMode';
+        console.log(currentMode);
+        canvas.isDrawingMode=true;
+        }
+    else {
+        currentMode = 'buildMode',
+        console.log(currentMode);
+        canvas.isDrawingMode=false;
+        }
+};
+
+//the setPanEvents include the mouse down, up, and moving parameters for when we are in pan mode
+function setMouseEvents(canvas) {
+    canvas.on("mouse:down", function (event) {
+        if (currentMode === modes.pan){
+            mousePressed = true,
+            console.log ("mouse was pressed in pan mode"),
+            canvas.setCursor ('grab'),
+            canvas.renderAll()
+        } else if (currentMode === modes.draw) {
+            mousePressed = true,
+            console.log ("mouse was pressed in draw mode"),
+            canvas.setCursor('crosshair'),
+            canvas.renderAll()
+        } else {
+            console.log ("mouse was pressed")
+        }
+    
+    })
+
+    canvas.on("mouse:move", function (event) {
+        if (mousePressed && currentMode === modes.pan) {
+            const mEvent = event.e;
+            const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
+            canvas.relativePan(delta);
+            console.log ("mouse is moving in pan mode");
+            canvas.setCursor ('grab');
+            canvas.renderAll()
+        } else if (mousePressed && currentMode === modes.draw ) {
+            console.log ("mouse is moving in draw mode");
+            canvas.renderAll()
+            }
+        
+    })
+    
+    canvas.on("mouse:up", function (event) {
+        mousePressed = false;
+        console.log("mouse is up");
+        canvas.setCursor ('default');
+        canvas.renderAll()
+    })
+         
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-// mouse over
-canvas.on("mouse:move", function (event) {
-    if (mousePressed && currentMode === modes.pan) {
-        const mEvent = event.e;
-        const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
-        canvas.relativePan(delta);
-        canvas.setCursor ('crosshair'),
-        canvas.renderAll()
-}})
 
-canvas.on("mouse:down", function (event) {
-    if (currentMode === modes.pan){
-        mousePressed = true,
-        console.log ("Mouse Pressed parameter is now :" + mousePressed),
-        canvas.setCursor ('grab'),
-        canvas.renderAll()
-    }
-
-})
-
-canvas.on("mouse:up", function (event) {
-    mousePressed = false,
-    console.log ("Mouse Pressed parameter is now :" + mousePressed)
-    canvas.setCursor ('default'),
-    canvas.renderAll()
-})
-
-
-/* mouse left the canvas
-canvas.on("mouse:out", () =>{
-    console.log ("the mouse left the canvas")
-})*/
-
-canvas.renderAll();
+setBackground ('https://cdn.pixabay.com/photo/2016/11/23/13/48/beach-1852945_960_720.jpg', canvas);
+setMouseEvents (canvas);
 
